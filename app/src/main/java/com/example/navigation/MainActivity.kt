76d8cd9bd99cf034534fc0aa32ebf4b3b7c38722
mahendrakarr <saichakradharrm@gmail.com>
2025-01-29 12:13:04 +0530

    Navigation

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.ui.theme.NavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        MyApp()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    var navController = rememberNavController()
+    NavHost(navController=navController, startDestination = "firstScreen"){
+        composable(route = "firstScreen"){
+            FirstScreen {name->
+                navController.navigate("secondScreen/$name")
+            }
+        }
+        composable(route = "secondScreen/{name}"){
+            var name = it.arguments?.getString("name") ?: "No Name"
+            SecondScreen(name) {
+                navController.navigate("firstScreen")
+            }
+        }
+    }
+}
